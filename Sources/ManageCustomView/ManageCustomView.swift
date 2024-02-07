52c4ec84fe181ca579dbff31956b30e8af71c08c
Mohamed Alsheikh <mohamed.alsheikh10@gmail.com>
2024-02-08 00:42:13 +0200

    Update DKImagePickerController hide and show take picture and record video Button

--- Sources/ManageCustomView/ManageCustomView.swift
+++ Sources/ManageCustomView/ManageCustomView.swift
@@ -11,24 +11,44 @@ class ManageCustomView: UIView, NibInstantiatable {
     @IBOutlet weak var LimittedPermisionView: UIView!
     @IBOutlet weak var LimittedPermisionLbl: UILabel!
     
+    @IBOutlet weak var cameraView: UIView!
     @IBOutlet weak var takePhotoBtn: UIButton!
     @IBOutlet weak var recordVideoBtn: UIButton!
     @IBOutlet weak var editLimitsBtn: UIButton!
     
     
-    static public func instance(hasLimitedAccess: Bool, frame: CGRect) -> ManageCustomView {
+    static public func instance(hasLimitedAccess: Bool, hidesCamera: Bool, hidesVideo: Bool, frame: CGRect) -> ManageCustomView {
        let view = ManageCustomView.fromNib()
         view.frame = frame
 
-        view.configView(hasLimitedAccess: hasLimitedAccess)
+        view.configView(hasLimitedAccess: hasLimitedAccess, hidesCamera: hidesCamera, hidesVideo: hidesVideo)
         return view
     }
     
-    func configView(hasLimitedAccess: Bool) {
+    func configView(hasLimitedAccess: Bool, hidesCamera: Bool, hidesVideo: Bool) {
+        configLimittedPermisionView(hasLimitedAccess: hasLimitedAccess)
+        configCameraView(hidesCamera: hidesCamera, hidesVideo: hidesVideo)
+    }
+    
+    func configLimittedPermisionView(hasLimitedAccess: Bool) {
         LimittedPermisionView.isHidden = !hasLimitedAccess
         LimittedPermisionLbl.text = DKImagePickerControllerResource.localizedStringWithKey("limited.photo.access")
+    }
+    
+    func configCameraView(hidesCamera: Bool, hidesVideo: Bool) {
+        if hidesCamera && hidesVideo {
+            cameraView.isHidden = true
+        } else if hidesCamera {
+            takePhotoBtn.isHidden = true
+        } else if hidesVideo {
+            recordVideoBtn.isHidden = true
+        }
+        
         takePhotoBtn.layer.cornerRadius = 12
         recordVideoBtn.layer.cornerRadius = 12
+        
+        takePhotoBtn.setTitle(DKImagePickerControllerResource.localizedStringWithKey("take.a.picture"), for: .normal)
+        recordVideoBtn.setTitle(DKImagePickerControllerResource.localizedStringWithKey("record.a.video"), for: .normal)
     }
     
 }
