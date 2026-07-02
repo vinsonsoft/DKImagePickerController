c3e160bd698e812ce98b95975f4e1774b66c391a
Mohamed Alsheikh <mohamed.alsheikh10@gmail.com>
2026-07-02 03:35:01 +0300

    Try another way to to load the view

--- Sources/ManageCustomView/ManageCustomView.swift
+++ Sources/ManageCustomView/ManageCustomView.swift
@@ -17,11 +17,32 @@ class ManageCustomView: UIView, NibInstantiatable {
     @IBOutlet weak var editLimitsBtn: UIButton!
     
     
-    static public func instance(hasLimitedAccess: Bool, hidesCamera: Bool, hidesVideo: Bool, frame: CGRect) -> ManageCustomView {
-       let view = ManageCustomView.fromNib()
+    static public func instance(
+        hasLimitedAccess: Bool,
+        hidesCamera: Bool,
+        hidesVideo: Bool,
+        frame: CGRect
+    ) -> ManageCustomView {
+        
+        let bundle = Bundle(for: ManageCustomView.self)
+        
+        guard let view = bundle.loadNibNamed(
+            "ManageCustomView",
+            owner: nil,
+            options: nil
+        )?.first as? ManageCustomView else {
+            
+            fatalError("Unable to load ManageCustomView.xib")
+        }
+        
         view.frame = frame
-
-        view.configView(hasLimitedAccess: hasLimitedAccess, hidesCamera: hidesCamera, hidesVideo: hidesVideo)
+        
+        view.configView(
+            hasLimitedAccess: hasLimitedAccess,
+            hidesCamera: hidesCamera,
+            hidesVideo: hidesVideo
+        )
+        
         return view
     }
     
